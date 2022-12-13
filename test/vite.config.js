a8256c55f727800ea799a3be5107a0b84d1167a2
Eduard Lik <feft99@gmail.com>
2022-12-14 02:30:33 +0600

    Fixing

--- test/vite.config.js
+++ test/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-import { unpack } from '../boot'
+import { unpack } from '../lib'
 
 export default defineConfig(params => {
   // process.env = { ...process.env, ...unpack(loadEnv(params.mode, process.cwd(), 'ENV_')) }
